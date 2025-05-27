d1bd01b38288a3e5334488494d8cefef7724eb5c
alkeshmahamune <alkeshmahamune@gmail.com>
2025-05-27 14:35:57 +0530

    hajksjd

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
-<<<<<<< HEAD
 import userRouter from './routers/user.routers.js'
 import cors from 'cors'
-=======
 import userRouter from './routers/user.routers.js';
 import adminRouter from './routers/admin.router.js'
->>>>>>> 97ffb0decc3c10c6920d21f29af75c6c6e84e525
 
 const app = express();
 
